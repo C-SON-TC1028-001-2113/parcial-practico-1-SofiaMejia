13b9da1d198827d2410c4499b3916150aade1699
SofiaMejia <A01252540@itesm.mx>
2021-09-10 16:43:27 +0000

    08 punto circunferencia

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,8 +1,19 @@
 
-
+import math 
 def main():
     # Escribe tu código abajo de esta línea
-   
+        r = float(input('Introduce el radio: '))
+        x1 = float(input('Introduce x1: '))
+        y1 = float(input('Introduce y1: '))
+        x2 = float(input('Introduce x2: '))
+        y2 = float(input('Introduce y2: '))
+        distancia = math.sqrt(((x2-x1)**2)+(y2-y1)**2)
+        if distancia ==r:
+            print(str('SOBRE'))
+        elif distancia <r:
+            print(str('DENTRO'))
+        else: 
+            print(str('FUERA'))
 
 if __name__ == '__main__':
     main()
